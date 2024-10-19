4c87630ca289b845bd3fb8bf4dd70108c4c23b0f
katebres <katebreslavskaya@gmail.com>
2024-10-19 11:49:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PM0XGSP2VX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PM0XGSP2VX');
+</script>
+
 let scrollAmount = 0;
 
 function scrollCarouselLeft() {
